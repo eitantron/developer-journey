9ea8ec6f500cd7b364167f46a36a88eba8a973ca
eitantron <eitanmalka07@gmail.com>
2026-04-30 08:26:02 -0700

    Improve budget checker with float instead of int

--- assignment1.py
+++ assignment1.py
@@ -1,8 +1,8 @@
 name = input("Enter your name: ")
-budget = input("Enter your budget: ")
-spent = input("Enter how much you have spent today: ")
+budget =float(input("Enter your budget: "))
+spent = float(input("Enter how much you have spent today: "))
 print(f"hello {name}! I hope you are having a good day")
-if int(budget) - int(spent) > 0:
-    print(f"you have this amount of money left over: {int(budget) - int(spent)}")
+if float(budget) - float(spent) > 0:
+    print(f"you have this amount of money left over: {float(budget) - float(spent)} dollars. You got this!")
 else:
-    print(f"You have gone over your budget by this much today: {int(budget) - int(spent)}")
+    print(f"You have gone over your budget by this much today: {float(budget) - float(spent)}")
