09166b102513a4fd69a7cbbe399bc1027cd7c1b5
eitantron <eitanmalka07@gmail.com>
2026-04-30 18:27:02 -0700

    finilzed budget checker with elif and positive ending

--- assignment1.py
+++ assignment1.py
@@ -1,8 +1,12 @@
 name = input("Enter your name: ")
 budget =float(input("Enter your budget: "))
 spent = float(input("Enter how much you have spent today: "))
+difference = budget - spent
 print(f"hello {name}! I hope you are having a good day")
-if float(budget) - float(spent) > 0:
-    print(f"you have this amount of money left over: {float(budget) - float(spent)} dollars. You got this!")
+if budget - spent > 0:
+    print(f"you have this amount of money left over: {budget - spent} dollars. Be good!")
+elif float(difference) == 0:
+    print(f"You are at $0 neither a defficit nor a surplus. You got this keep on tracking!")
 else:
-    print(f"You have gone over your budget by this much today: {float(budget) - float(spent)}")
+    print(f"You have gone over your budget by this much today: {-difference} You got this be better tomorrow.")
+print("You will be better tomorrow")
