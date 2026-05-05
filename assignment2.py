4960dbaf127bf1f42d40b4cc1bc9e9d4bc3394ee
eitantron <eitanmalka07@gmail.com>
2026-05-04 21:19:19 -0700

    Made it so the times add up

--- assignment2.py
+++ assignment2.py
@@ -1,23 +1,30 @@
-""" total_minutes = 0 
-habit_log = "\n"
-while True:
+total_minutes = 0 
+habit_log = ("\n")
+habit_list = []
+time_list = []
+flag = True
+while flag:
     print("welcome to our tiny habit tracker")
-    print("1) log a new habit")
+    print("1) add a new habit")
     print("2) Show total minutes")
     print("3) show habit list")
     print("4) Quit")
-choice = input("Pick 1-4: ")
-if choice == "4":
-    print("Goodbye")
-elif choice == "3":
-    print("TODO: list next")
-elif choice == "2":
-    print("TODO: total next")
-elif choice == "1":
-    print("TODO: logging next")
-else:
-    print("Your choice is invalid")
-quit
-"""
-print("5" + "7")
-print(5 + 7)
+    choice = input("Pick 1-4: ")
+    if choice == "4":
+        print("Goodbye")
+        flag = False
+    elif choice == "3":
+        print("TODO: list next")
+    elif choice == "2":
+        print(total_minutes)
+    elif choice == "1":
+        choice_1 = input("What Habbit are you tracking: ")
+        choice_1a = int(input("For how long did you do this: "))
+        habit_list.append(choice_1)
+        time_list.append(choice_1a)
+        total_minutes = total_minutes + choice_1a
+        habit_log = habit_log + choice_1 + "\n"
+        print(habit_list)
+        print(time_list)
+    else:
+        print("Your choice is invalid")
