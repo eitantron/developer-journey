94b8233cf81cf2cbd4908cba377323d4f4d8fa83
eitantron <eitanmalka07@gmail.com>
2026-05-04 20:46:28 -0700

    I learnt something new

---
+++ assignment2.py
@@ -0,0 +1,23 @@
+""" total_minutes = 0 
+habit_log = "\n"
+while True:
+    print("welcome to our tiny habit tracker")
+    print("1) log a new habit")
+    print("2) Show total minutes")
+    print("3) show habit list")
+    print("4) Quit")
+choice = input("Pick 1-4: ")
+if choice == "4":
+    print("Goodbye")
+elif choice == "3":
+    print("TODO: list next")
+elif choice == "2":
+    print("TODO: total next")
+elif choice == "1":
+    print("TODO: logging next")
+else:
+    print("Your choice is invalid")
+quit
+"""
+print("5" + "7")
+print(5 + 7)
---
+++ lesson1.py
@@ -0,0 +1,4 @@
+name="eitan"
+favFood = "Sushi"
+sentence = "Barrow is teaching me python"
+print("Hello my name is", name, "and my favorite food is", favFood, "and", sentence)
